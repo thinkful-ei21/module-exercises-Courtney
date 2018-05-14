cc8e3c93a7c8afd6f59c1b52c8a75bb5be39d036
Courtney Adams <cadams8238@gmail.com>
2018-05-14 09:03:35 -0700

    removed testing for store.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,8 @@
 
 'use strict';
 
-// eslint-disable-next-line no-unused-vars
-// const store = {
-//   items: [
-//     { id: cuid(), name: 'apples', checked: false },
-//     { id: cuid(), name: 'oranges', checked: false },
-//     { id: cuid(), name: 'milk', checked: true },
-//     { id: cuid(), name: 'bread', checked: false }
-//   ],
-//   hideCheckedItems: false,
-//   searchTerm: ''
-// };
 
 $(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
-  console.log(store);
-  console.log(foo);
 });
--- scripts/store.js
+++ scripts/store.js
@@ -1,11 +1,10 @@
-/* global store */
+/* global store, cuid */
 
 'use strict';
 
 //eslint-disable-next-line no-unused-vars
 const store = (function() {
 
-	const foo = 'bar';
 	const items = [
 	    { id: cuid(), name: 'apples', checked: false },
 	    { id: cuid(), name: 'oranges', checked: false },
