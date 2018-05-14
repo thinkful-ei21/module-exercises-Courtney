f17ddfc8ee1e1450733d2b5bb2e00376b269b2c9
Courtney Adams <cadams8238@gmail.com>
2018-05-14 09:40:45 -0700

    added validateName and create methods to Item module, tested but removed test code

---
+++ scripts/Item.js
@@ -0,0 +1,27 @@
+/* global Item */
+
+'use strict';
+
+//eslint-disable-next-line no-unused-vars
+const Item = (function() {
+
+	const validateName = function(name) {
+		if (!name) {
+			throw new TypeError("Name does not exist");
+		}
+	};
+
+	const create = function(name) {
+		return {
+			id: cuid(),
+			name: name,
+			checked: false
+		};
+	};
+
+	return {
+		validateName: validateName,
+		create: create
+	};
+	
+}());
